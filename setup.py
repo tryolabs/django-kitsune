ed0724d51d86744efac3793464c31301e8e2b91b
Alejandro <ale.martinez.uy@gmail.com>
2014-05-08 02:14:24 -0300

    Added requirement to setup.py: python-dateutil

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ Created on Mar 31, 2012
 
 from setuptools import setup, find_packages
 
-setup(name='django-kitsune',
-      version=__import__('kitsune').get_version(limit=3),
-      description='A Django Admin app to perform host server monitoring.',
-      author='Raul Garreta - Tryolabs',
-      author_email='raul@tryolabs.com',
-      url='https://github.com/tryolabs/django-kitsune',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False
-     )
-
+setup(
+    name='django-kitsune',
+    version=__import__('kitsune').get_version(limit=3),
+    description='A Django Admin app to perform host server monitoring.',
+    author='Raul Garreta - Tryolabs',
+    author_email='raul@tryolabs.com',
+    url='https://github.com/tryolabs/django-kitsune',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'python-dateutil',
+    ],
+    zip_safe=False
+)
