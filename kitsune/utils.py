368b6d0a762afc1b14012c1477dc80db2c773dbd
Alejandro <ale.martinez.uy@gmail.com>
2014-05-08 02:44:43 -0300

    Quick and dirty patch for django 1.6 compatibility. This MUST be fixed in a better way.

--- kitsune/utils.py
+++ kitsune/utils.py
@@ -8,15 +8,26 @@ Based on django-chronograph.
 
 '''
 
-__author__      = "Raul Garreta (raul@tryolabs.com)"
+__author__ = "Raul Garreta (raul@tryolabs.com)"
 
-
-from django.conf import settings
-from django.core.management import setup_environ
-from django.utils.importlib import import_module
 
 import os
 
+import django
+from django.conf import settings
+from django.utils.importlib import import_module
+
+
 def get_manage_py():
     module = import_module(settings.SETTINGS_MODULE)
-    return os.path.join(setup_environ(module, settings.SETTINGS_MODULE), 'manage.py')
+    if django.get_version().startswith('1.3'):
+        # This is dirty, but worked in django <= 1.3 ...
+        from django.core.management import setup_environ
+        return os.path.join(
+            setup_environ(module, settings.SETTINGS_MODULE), 'manage.py'
+        )
+    else:
+        # Dirty again, but this should work in django > 1.3
+        # We should DEFINITELY do this in an elegant way ...
+        settings_path = os.path.dirname(module.__file__)
+        return os.path.join(settings_path, '..', 'manage.py')
